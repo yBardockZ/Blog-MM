a0955ff43a24d045119573307fd1337ee9829699
Thalles <thalles_leopoldino@outlook.com>
2025-09-25 18:27:09 -0300

    Projeto e migrations criadas

---
+++ database/migrations/2025_09_25_204649_create_comments_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('comments', function (Blueprint $table) {
+            $table->id();
+            $table->text('content');
+            $table->foreignId('post_id')->constrained('posts')->onDelete('cascade');
+            $table->string('author_name');
+            $table->timestamp('created_at')->useCurrent();
+
+            $table->foreign('author_name')
+            ->references('name')
+            ->on('users')
+            ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('comments');
+    }
+};
