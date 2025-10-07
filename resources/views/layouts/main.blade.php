b6bd923576f8179452bbad234ed3474f6e0482d5
Thalles <thalles_leopoldino@outlook.com>
2025-10-06 21:20:14 -0300

    Scaffolding breeze instalado e tailwind removido

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -13,8 +13,7 @@
     {{-- NOVO: Bootstrap Icons CSS --}}
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
 
-
-    <link rel="stylesheet" href="{{ asset('css/styles.css') }}">
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 
     <title>@yield('title')</title>
 </head>
--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,9 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\CommentController;
-use Illuminate\Support\Facades\Route;
-
 use App\Http\Controllers\PostController;
+use Illuminate\Support\Facades\Route;
 
 Route::get('/', [PostController::class, 'index'])->name('posts.index');
 Route::get('/posts/create', [PostController::class, 'create'])->name('posts.create');
@@ -13,3 +13,15 @@ Route::get('/posts/{id}', [PostController::class, 'show'])->name('posts.show');
 Route::post('/comments', [CommentController::class, 'store'])->name('comments.store');
 Route::delete('/comments/{id}', [CommentController::class, 'destroy'])->name('comments.destroy');
 Route::patch('/comments/{id}', [CommentController::class, 'update'])->name('comments.update');
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
