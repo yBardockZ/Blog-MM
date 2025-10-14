b459def0daf040df852daf3b6bc55b65576474c8
Thalles <thalles_leopoldino@outlook.com>
2025-10-13 23:50:10 -0300

    Ajustando as animações do hero-banner

--- resources/views/include/hero-banner.blade.php
+++ resources/views/include/hero-banner.blade.php
@@ -1,40 +1,78 @@
 <section class="hero-banner container-fluid p-0 d-flex flex-column justify-content-center align-items-center text-center text-light">
-        <div class="hero-overlay"></div>
-
-        <div class="hero-content position-relative" 
-        x-data="{ 
-            showTitle: false, 
-            showSubtitle: false 
-        }" 
-        x-init="
-            setTimeout(() => showTitle = true, 400);
-            setTimeout(() => showSubtitle = true, 400);
-        ">
-            <h1 x-show="showTitle"
-            x-transition:enter="ease-out duration-1000"
-            x-transition:enter-start="opacity-0 translate-y-4"
-            x-transition:enter-end="opacity-100 translate-y-0"
-            class="animate-element">
-                Bem-vindo ao Blog!
-            </h1>
+    <div class="hero-overlay"></div>
     
-            <p x-show="showSubtitle"
-            x-transition:enter="ease-out duration-1000"
-            x-transition:enter-start="opacity-0 translate-y-4"
-            x-transition:enter-end="opacity-100 translate-y-0"
-            class="animate-element">
-                Aqui você encontra as últimas novidades...
-            </p>
-
+    <div class="hero-content position-relative">
+        
+        @if(!request('search'))
+            {{-- COM ANIMAÇÃO - Apenas quando NÃO há busca --}}
+            
+            {{-- Título --}}
+            <div x-data="{ show: false }" 
+                 x-init="setTimeout(() => show = true, 200)"
+                 x-cloak>
+                <h1 x-show="show"
+                    x-transition.opacity.duration.1000ms
+                    style="transition: opacity 1s ease-out;">
+                    Bem-vindo ao Blog!
+                </h1>
+            </div>
+            
+            {{-- Parágrafo --}}
+            <div x-data="{ show: false }" 
+                 x-init="setTimeout(() => show = true, 600)"
+                 x-cloak>
+                <p x-show="show"
+                   x-transition.opacity.duration.1000ms
+                   style="transition: opacity 1s ease-out;">
+                    Aqui você encontra as últimas novidades...
+                </p>
+            </div>
+            
             {{-- Barra de pesquisa --}}
-            <form action="{{ route('posts.index') }}" method="GET" class="d-flex justify-content-center">
-                <div class="input-group w-200">
-                    <input type="text" name="search" class="form-control" placeholder="Pesquisar posts..."
-                        value="{{ request('search') }}">
+            <form action="{{ route('posts.index') }}" 
+                  method="GET" 
+                  class="d-flex justify-content-center">
+                
+                <div x-data="{ show: false }" 
+                     x-init="setTimeout(() => show = true, 1000)"
+                     x-cloak
+                     class="input-group w-200"
+                     x-show="show"
+                     x-transition.opacity.duration.1000ms
+                     style="transition: opacity 1s ease-out;">
+                    
+                    <input type="text" 
+                           name="search" 
+                           class="form-control" 
+                           placeholder="Pesquisar posts..."
+                           value="{{ request('search') }}">
                     <button class="btn btn-outline-light" type="submit">
                         <i class="bi bi-search"></i> Pesquisar
                     </button>
                 </div>
             </form>
-        </div>
+            
+        @else
+            {{-- SEM ANIMAÇÃO - Quando há busca ativa --}}
+            
+            <h1>Bem-vindo ao Blog!</h1>
+            <p>Aqui você encontra as últimas novidades...</p>
+            
+            <form action="{{ route('posts.index') }}" 
+                  method="GET" 
+                  class="d-flex justify-content-center">
+                
+                <div class="input-group w-200">
+                    <input type="text" 
+                           name="search" 
+                           class="form-control" 
+                           placeholder="Pesquisar posts..."
+                           value="{{ request('search') }}">
+                    <button class="btn btn-outline-light" type="submit">
+                        <i class="bi bi-search"></i> Pesquisar
+                    </button>
+                </div>
+            </form>
+        @endif
+    </div>
 </section>
